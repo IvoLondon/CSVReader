eee02f65555117f861d9e226e56aaeae4a6a89ce
Iwi4a <ivelin.iliev@hotmail.com>
2017-08-14 23:20:06 +0100

    Pie chart added

--- js/script.js
+++ js/script.js
@@ -1,46 +1,127 @@
 'use strict';
+function handleFileSelect(evt) {
+  var file = evt.target.files[0];
+  var reader = new FileReader();
+  reader.onload = (function(theFile) {
+    return function(e) {
 
-var reader = new FileReader();  
-    
-function loadFile() {      
-  var file = document.querySelector('input[type=file]').files[0];      
-  reader.addEventListener("load", function() {
-    var ar = reader.result;
-    var arrayLines = ar.split('\n');
-    var headers = arrayLines[0].split(',');
-    var tableHeader = document.querySelector('#contentTable thead tr');
-    var tableBody = document.querySelector('#contentTable tbody');
-    var tableBodyContent = '';
+      /* print the data as a table */
+      var openFile = new XMLHttpRequest();
+      openFile.open('GET', e.target.result, true);
+      openFile.responseType = 'text';
+      openFile.onload = function() {
+        if(openFile.status === 200) {
+          var obj = {};
+          var jsonObj = [];
+          var ar = openFile.response;
+          var arrayLines = ar.split('\n');
+          var headers = arrayLines[0].split(',');
+          var tableHeader = document.querySelector('#contentTable thead tr');
+          var tableBody = document.querySelector('#contentTable tbody');
+          var tableBodyContent = '';
 
-    /* create header */
-    for(var h = 0; h < headers.length; h++) {
-      tableHeader.innerHTML += '<td>' + headers[h] + '</td>';
-    }
+          /* table - create header */
+          for(var h = 0; h < headers.length; h++) {
+            tableHeader.innerHTML += '<td>' + headers[h] + '</td>';
+          }
 
-    /* create body + obj */
-    for(var i = 1; i < arrayLines.length; i++) {
-      var obj = {};
-      var data = arrayLines[i].split('\,');
-      
-      /* obj */
-      for(var j = 0; j < data.length; j++) {
-        obj[myTrim(headers[j])] = myTrim(data[j]);
+          /* table - create body */
+          for(var i = 1; i < (arrayLines.length - 1); i++) {
+            var data = arrayLines[i].split('\,');
+            tableBodyContent += '<tr>';
+            for(var t = 0; t < data.length; t++) {
+              tableBodyContent += '<td>' + myTrim(data[t]) + '</td>';
+            }
+            tableBodyContent += '</tr>';
+          }
+          tableBody.innerHTML = tableBodyContent;
+        }
       }
+      openFile.send();
 
-      /* table */
-      tableBodyContent += '<tr>';
-      for(var t = 0; t < data.length; t++) {
-        tableBodyContent += '<td>' + myTrim(data[t]) + '</td>';
-      }
-      tableBodyContent += '</tr>';
-    }
-    
-    tableBody.innerHTML = tableBodyContent;
-  }, false);
-  reader.readAsBinaryString(file);     
+
+      /* send link to draw the chart */
+      drawChart(e.target.result);
+    };
+  })(file);
+
+  reader.readAsDataURL(file);
 }
 
+document.getElementById('file_input').addEventListener('change', handleFileSelect, false);
 
+/* draw pie chart */
+function drawChart(url) {
+  var width = 360;
+  var height = 360;
+  var radius = Math.min(width, height) / 2;
+  var donutWidth = 75;
+  var legendRectSize = 18;
+  var legendSpacing = 4;
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
+  var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) +
+      ',' + (height / 2) + ')');
+
+  var arc = d3.arc()
+    .innerRadius(radius - donutWidth)
+    .outerRadius(radius);
+
+  var pie = d3.pie()
+    .value(function(d) {
+      return d.LandArea;
+    })
+    .sort(null);
+
+  d3.csv(url, function(error, dataset) {
+    dataset.forEach(function(d) {
+      d.LandArea = +d.LandArea;
+    });
+
+    var path = svg.selectAll('path')
+      .data(pie(dataset))
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', function(d, i) {
+        return color(i);
+      });
+    var legend = svg.selectAll('.legend')
+      .data(dataset)
+      .enter()
+      .append('g')
+      .attr('class', 'legend')
+      .attr('transform', function(d, i) {
+        var height = legendRectSize + legendSpacing;
+        var offset = height * color(d).length / 2;
+        var horz = -2 * legendRectSize;
+        var vert = i * height - offset;
+        return 'translate(' + horz + ',' + vert + ')';
+      });
+    legend.append('rect')
+      .attr('width', legendRectSize)
+      .attr('height', legendRectSize)
+      .style('fill', function(d, i) {
+        return color(i);
+      })
+      .style('stroke', function(d, i) {
+        return color(i);
+      });
+    legend.append('text')
+      .attr('x', legendRectSize + legendSpacing)
+      .attr('y', legendRectSize - legendSpacing)
+      .text(function(d, i) {
+        return d.City;
+      })
+      .style('fill', '#fff');
+  });
+}
+
+/* trim empty spaces */
 function myTrim(x) {
   x.replace(/^\s+|\s+$/gm,'');
   var trimVal = parseFloat(x);
@@ -50,3 +131,4 @@ function myTrim(x) {
     return trimVal;
   }
 }
+
