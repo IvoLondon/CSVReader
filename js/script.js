25de2f615a031bef51ba5a44450ac22bf527b958
Iwi4a <ivelin.iliev@hotmail.com>
2017-08-15 00:11:07 +0100

    Bar chart added

--- js/script.js
+++ js/script.js
@@ -40,12 +40,23 @@ function handleFileSelect(evt) {
       openFile.send();
 
 
-      /* send link to draw the chart */
+      /* send link to draw the charts */
       drawChart(e.target.result);
+      drawBar(e.target.result);
     };
   })(file);
 
   reader.readAsDataURL(file);
+}
+/* trim empty spaces */
+function myTrim(x) {
+  x.replace(/^\s+|\s+$/gm,'');
+  var trimVal = parseFloat(x);
+  if(isNaN(trimVal)) {
+    return x;
+  } else {
+    return trimVal;
+  }
 }
 
 document.getElementById('file_input').addEventListener('change', handleFileSelect, false);
@@ -53,7 +64,7 @@ document.getElementById('file_input').addEventListener('change', handleFileSelec
 /* draw pie chart */
 function drawChart(url) {
   var width = 360;
-  var height = 360;
+  var height = 420;
   var radius = Math.min(width, height) / 2;
   var donutWidth = 75;
   var legendRectSize = 18;
@@ -67,6 +78,13 @@ function drawChart(url) {
     .attr('transform', 'translate(' + (width / 2) +
       ',' + (height / 2) + ')');
 
+  svg.append('text')
+    .attr("y", (-height/2) + 20)
+    .attr("text-anchor", "middle") 
+    .text('Land Area')
+    .style("font-size", "20px") 
+    .style('fill', '#fff');
+
   var arc = d3.arc()
     .innerRadius(radius - donutWidth)
     .outerRadius(radius);
@@ -76,6 +94,7 @@ function drawChart(url) {
       return d.LandArea;
     })
     .sort(null);
+    
 
   d3.csv(url, function(error, dataset) {
     dataset.forEach(function(d) {
@@ -121,14 +140,78 @@ function drawChart(url) {
   });
 }
 
-/* trim empty spaces */
-function myTrim(x) {
-  x.replace(/^\s+|\s+$/gm,'');
-  var trimVal = parseFloat(x);
-  if(isNaN(trimVal)) {
-    return x;
-  } else {
-    return trimVal;
-  }
+function drawBar(url) {
+  // set the dimensions and margins of the graph
+  var margin = {top: 150, right: 20, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
+  // set the ranges
+  var x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.1);
+  var y = d3.scaleLinear()
+            .range([height, 0]);
+            
+  // append the svg object to the body of the page
+  // append a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("#bar").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append('text')
+    .attr("y", 0)
+    .attr('x', width/2)
+    .attr("text-anchor", "middle") 
+    .text('Population (Millions)')
+    .style("font-size", "20px") 
+    .style('fill', '#fff');
+
+  // get the data
+  d3.csv(url, function(error, data) {
+    if (error) throw error;
+
+    // format the data
+    data.forEach(function(d) {
+      d.Population = +d.Population;
+    });
+
+    // Scale the range of the data in the domains
+    x.domain(data.map(function(d) { return d.City; }));
+    y.domain([0, d3.max(data, function(d) {
+        return d.Population;
+      })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.City); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.Population); })
+      .attr("height", function(d) { return height - y(d.Population); })
+      .style('fill', function(d, i) {
+        return color(i);
+      });
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")",)
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+  });
 }
+
+
+
+
+
 
